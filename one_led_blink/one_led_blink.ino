6f9de8766cf8a03951387de00cecb5c0f1ffea6b
Williano <paawilly17@gmail.com>
2020-02-21 14:43:50 -0600

    Add one_led_blink project

---
+++ one_led_blink/one_led_blink.ino
@@ -0,0 +1,24 @@
+/*
+ * Sketch: One_led_blink
+ * Description: A Sketch in C to have an LED connected to an output pin blinking with the frequency of 1Hz.
+ * Author: William Kwabla
+ * Date: 02/21/20
+*/
+
+
+int ledOutPutPin = 8; // Declare pin to use for output.
+
+void setup() {
+  // put your setup code here, to run once:  
+  pinMode(ledOutPutPin, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  digitalWrite(ledOutPutPin, HIGH); // Turn the led on(HIGH is the voltage on)
+  delay(1000); // Wait for 1Hz
+  digitalWrite(ledOutPutPin, LOW); // Turn the led off
+  delay(1000);
+}
