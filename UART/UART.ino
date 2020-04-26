f85aace75c9aa9d3d9761ba13fc120701317ac42
Williano <paawilly17@gmail.com>
2020-04-26 00:01:43 -0500

    Update

---
+++ UART/UART.ino
@@ -0,0 +1,32 @@
+/*
+ * Sketch: UART
+ * Description: A Sketch in C to send data to another Arduino.
+ * Author: William Kwabla
+ * Date: 03/25/20
+*/
+
+int led = 13;
+int value = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+
+  if (Serial.available() > 0) {
+    value = Serial.read();
+    delay(5);
+
+    if (value == '1') {
+      digitalWrite(led, HIGH);
+      Serial.println("LED is ON");
+    }
+
+    if (value == '0') {
+      digitalWrite(led, LOW);
+      Serial.println("LED is OFF");
+    }
+  }
+}
