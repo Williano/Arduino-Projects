3928500d3e1a227f9490af12a202a5a8d8f9dc31
Williano <paawilly17@gmail.com>
2020-04-25 23:49:07 -0500

    Update

--- pwm_sine_wave/pwm_sine_wave.ino
+++ pwm_sine_wave/pwm_sine_wave.ino
@@ -1,19 +1,68 @@
-void setup() {
-  // put your setup code here, to run once:
+/*
+  Sketch: Arduino Uno sine wave generator
+  
+*/
+
+
+const int sPWMArray[] = {500,500,750,500,1250,500,2000,500,1250,500,750,500,500}; // This is the array with the SPWM values change them at will
+const int sPWMArrayValues = 13; 
+
+// The pins
+const int sPWMpin1 = 10;
+const int sPWMpin2 = 9;
+// The pin switches
+bool sPWMpin1Status = true;
+bool sPWMpin2Status = true;
+
+
+void setup()
+{
+pinMode(sPWMpin1, OUTPUT);
+pinMode(sPWMpin2, OUTPUT);
+}
+
+
+
+
+void loop()
+{
+
+// Loop for pin 1
+for(int i(0); i != sPWMArrayValues; i++)
+{
+    if(sPWMpin1Status)
+{
+
+    digitalWrite(sPWMpin1, HIGH);
+    delayMicroseconds(sPWMArray[i]);
+
+     sPWMpin1Status = false;
+}
+else
+{
+  
+    digitalWrite(sPWMpin1, LOW);
+    delayMicroseconds(sPWMArray[i]);
+    sPWMpin1Status = true;
+}
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  analogWrite(9, 64);
-  delay(1000);
-  analogWrite(9, 127);
-  delay(1000);
-  analogWrite(9, 191);
-  delay(1000);
-  analogWrite(9, 255);
-  delay(1000);
-  analogWrite(9, 0);
-  delay(1000); 
+// Loop for pin 2
+for(int i(0); i != sPWMArrayValues; i++)
+{
+  if(sPWMpin2Status)
+  {
+    digitalWrite(sPWMpin2, HIGH);
+    delayMicroseconds(sPWMArray[i]);
+    sPWMpin2Status = false;
+   }
+else
+{
 
+   digitalWrite(sPWMpin2, LOW);
+   delayMicroseconds(sPWMArray[i]);
+   sPWMpin2Status = true;
+  }
+ }
 }
