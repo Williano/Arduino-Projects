2fc8edfcbecbf0a2bd82e80e012799a1a05851ee
Williano <paawilly17@gmail.com>
2020-06-18 17:38:48 -0500

    Update

--- lvdt_v2/lvdt_v2.ino
+++ lvdt_v2/lvdt_v2.ino
@@ -1,9 +1,12 @@
+
+int sensorPin1 = A0;
+int sensorPin2 = A1;
+
 void setup()
 {
 Serial.begin(9600); 
-pinMode(A0,INPUT); 
-pinMode(A1,INPUT);
 }
+
 void loop()
 {
   float secondary1 =0;
@@ -16,24 +19,29 @@ void loop()
   float dis = 0;
   int wave;
 
-// for(int i =0; i<=40; i++)
-// {
-//   secondary1 = sq(3.3 - analogRead(A0)*0.0048); 
-//   secondary2 = sq(3.3 - analogRead(A1)*0.0048); 
-//   secondary12 =secondary12+secondary1; 
-//   secondary22 =secondary22+secondary2; 
-//   delay(0.5);
-// }
-//   secondary13 =sqrt(secondary12/40); 
-//   secondary23 =sqrt(secondary22/40);
-//   dis = (125*(secondary13 - secondary23)); 
-//  Serial.println(dis);
+ for(int i =0; i<=40; i++)
+ {
+   secondary1 = sq(3.3 - analogRead(A0)*0.0048); 
+   secondary2 = sq(3.3 - analogRead(A1)*0.0048); 
+   secondary12 =secondary12+secondary1; 
+   secondary22 =secondary22+secondary2; 
+   delay(0.5);
+ }
+   secondary13 =sqrt(secondary12/40); 
+   secondary23 =sqrt(secondary22/40);
+   dis = (125*(secondary13 - secondary23)); 
+  Serial.println(dis);
 //  Serial.println(analogRead(A0));
 //  Serial.println(analogRead(A1));
 
-  Serial.println(analogRead(A0));
-  delay(20);
-  Serial.println(analogRead(A1));
+//  delay(50);
+// // Serial.println("A0");
+//  Serial.println(analogRead(sensorPin1));
+// // Serial.println("End");
+//  delay(50);
+// // Serial.println("A1");
+//  Serial.println(analogRead(sensorPin2));
+//  delay(50);
  
  
 }
